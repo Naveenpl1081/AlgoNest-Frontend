c2ecd345265b9b60919af17e55d03a7ea89f91bb
Naveenpl1081 <123857457+Naveenpl1081@users.noreply.github.com>
2025-11-20 16:36:37 +0530

    implement the live like function

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-e7681877'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kcrn9ebo3co"
+    "revision": "0.3nelq8c78h8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
